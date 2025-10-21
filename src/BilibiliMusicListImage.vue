b36b7c37faccddd7771bf13d44518603531d0d1c
yanhan <yanhan569046480@gmail.com>
2025-10-21 09:33:32 +0800

    .

--- src/BilibiliMusicListImage.vue
+++ src/BilibiliMusicListImage.vue
@@ -1,21 +1,12 @@
 <template>
   <div class="flex justify-center mb-4">
     <div class="w-[400px]">
-      <el-autocomplete
+      <el-input
           v-model="searchText"
-          :fetch-suggestions="handleFetchSuggestions"
-          :trigger-on-focus="false"
+          class="responsive-input"
+          :prefix-icon="Search"
           clearable
-          @select="handleSelect"
-          @clear="handleClear"
-      >
-        <template #prefix>
-          <svg class="w-5 h-5 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/>
-          </svg>
-        </template>
-      </el-autocomplete>
+      />
     </div>
   </div>
 
@@ -43,37 +34,31 @@
         prev-text="上一页"
         next-text="下一页"
     >
-      <template #total="{ total }">
-        <span>总记录：{{ total }} 条数据</span>
-      </template>
     </el-pagination>
   </div>
 </template>
 
 <script setup lang="ts">
 import musics from '@/musics.json'
-import {ref, computed, onMounted} from 'vue'
+import {ref, computed, watch} from 'vue'
+import {Search} from '@element-plus/icons-vue'
 
 const searchText = ref('')
 
 const background = ref(true)
 
 const allData = ref(musics);
-const filterdData = ref([]);
-const currentData = ref([]);
+const filterdData = ref(musics);
 
-const currentPage = ref(1);
 const pageSize = ref(parseInt(import.meta.env.VITE_PAGE_SIZE));
-const pageCount = ref(allData.value.length/pageSize.value + 1);
+const pageCount = computed(() => {
+  return parseInt(filterdData.value.length / pageSize.value) + 1;
+});
 const pagerCount = ref(parseInt(import.meta.env.VITE_PAGER_COUNT));
 
-const handleFetchSuggestions = (queryString, cb) => {
-  const filtered = queryString
-      ? allData.value.filter(createFilter(queryString))
-      : allData.value
-  const results = filtered.map(item => ({value: item}));
-  cb(results)
-}
+const currentPage = ref(1);
+const currentData = ref([]);
+
 const createFilter = (queryString) => {
   return (music) => {
     return (
@@ -82,33 +67,23 @@ const createFilter = (queryString) => {
   }
 }
 
+watch([() => searchText.value, () => currentPage.value, () => pageSize.value], ([newSearchText, newCurrentPage, newPageSize]) => {
+      filterdData.value = newSearchText ? allData.value.filter(createFilter(newSearchText)) : allData.value
+      const startIndex = (newCurrentPage - 1) * pageSize.value;
+      const endIndex = startIndex + newPageSize;
+      currentData.value = filterdData.value.slice(startIndex, endIndex);
+    },
+    {deep: true, immediate: true}
+)
+
 const handleSizeChange = (val) => {
   pageSize.value = val;
   currentPage.value = 1;
-  handleSelect(searchText);
 };
 
 const handleCurrentChange = (val) => {
   currentPage.value = val;
-  handleSelect(searchText);
 };
-
-const handleSelect = (searchText) => {
-  filterdData.value = searchText ? allData.value.filter(createFilter(searchText.value)) : allData.value
-  const startIndex = (currentPage.value - 1) * pageSize.value;
-  const endIndex = startIndex + pageSize.value;
-  currentData.value = filterdData.value.slice(startIndex, endIndex);
-
-  pageCount.value = parseInt(filterdData.value.length/pageSize.value) + 1;
-}
-
-const handleClear = () => {
-  handleSelect("");
-}
-
-onMounted(() => {
-  handleSelect(searchText);
-})
 </script>
 
 <style scoped>
